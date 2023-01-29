7b018afdb4448d7cb9a4359280af711e3e475e13
maythanunR <genxiuyin29@gmail.com>
2023-01-29 23:51:44 +0700

    29/01/2565

--- myapp/lib/test.dart
+++ myapp/lib/test.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/page/info.dart';
-import 'package:myapp/tournament/content2.dart';
-import 'package:myapp/team/content3.dart';
-import 'package:myapp/match/content4.dart';
-import 'package:myapp/rank/content5.dart';
-import 'package:myapp/highlight/content6.dart';
+
 
 class test extends StatefulWidget {
   const test({super.key});
@@ -16,70 +11,8 @@ class test extends StatefulWidget {
 class _testState extends State<test> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 6,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 3, 42, 121),
-          title: Text(
-            "การแข่งขันกีฬาฟุตบอลประจำปี 2565",
-            style: TextStyle(fontSize: 18),
-          ), //แก้ไขหัวข้อเปลี่ยนตามกีฬาที่กด
-          leading: BackButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          bottom: TabBar(
-            isScrollable: true,
-            tabs: [
-              Container(
-                width: 80,
-                child: Tab(
-                  text: 'ข้อมูล',
-                  icon: Icon(Icons.info),
-                ),
-              ),
-              Tab(
-                text: 'สายการแข่งขัน',
-                icon: Icon(Icons.star),
-              ),
-              Tab(
-                text: 'ทีมที่เข้าร่วม',
-                icon: Icon(Icons.group),
-              ),
-              Tab(
-                text: 'แมตช์การแข่งขัน',
-                icon: Icon(Icons.event),
-              ),
-              Container(
-                width: 80,
-                child: Tab(
-                  text: 'อันดับ',
-                  icon: Icon(Icons.military_tech),
-                ),
-              ),
-              Container(
-                width: 80,
-                child: Tab(
-                  text: 'ไฮไลต์',
-                  icon: Icon(Icons.smart_display),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            info(),
-            content2(),
-            content3(),
-            content4(),
-            content5(),
-            content6(),
-          ],
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
